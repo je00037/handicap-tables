1050fb45cec7e478f225a4159b09c276faa1bd67
Joe Ellis <joe.ellis@and.digital>
2021-10-10 19:16:41 +0100

    further tidying and structuring

---
+++ src/components/App.js
@@ -0,0 +1,5 @@
+import Header from './Header.js';
+
+const App = () => <Header />
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+const Header = () => (
+    <div>
+        <p>Handicap Tables will appear here very soon...</p>
+    </div>
+)
+
+export default Header;
