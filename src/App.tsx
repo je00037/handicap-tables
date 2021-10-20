c94b78e5203d461e60e4ae7791f93f62f7bb34a9
Joe Ellis <joe.ellis@and.digital>
2021-10-20 09:15:46 +0100

    added click handler to bookie button pills

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,14 @@ import Standings from './components/Standings';
 import Loading from './components/Loading';
 import BookiePicker from './components/BookiePicker';
 
-const App: any = () => {
+const App: FC = () => {
+
+    // add a state variable to track the selected bookie. Select Sky Bet by default
+    // add a new function like onClick which takes an argument of a new bookie and updates the selected bookie state
+    // pass this onClick function to the Bookie Picker as a prop
+    // pass the currently selected bookie as a prop to Standings and to Bookie Picker so the selected button style can be updated
+    // Standings will use that selected bookie for retrieving the handicap values in getStandingsArray
+    // in Bookie Picker each button needs to call onClick with the dynamic value of that button, the argument passed to it being a variable which holds the value of the button?
 
     let dataPlaceholder: any;
 
@@ -24,6 +31,11 @@ const App: any = () => {
 
     const [apiData, setApiData] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
+    const [currentBookie, setCurrentBookie] = useState('Sky Bet');
+
+    const clickHandler = (newBookie: string) => {
+        setCurrentBookie(newBookie);
+    }
 
     useEffect (() => {
         if (isLoading === true) {
@@ -34,7 +46,7 @@ const App: any = () => {
     return isLoading ? <Loading /> :
     <div className='h-screen flex flex-col justify-between items-center'>
         <Header />
-        <BookiePicker />
+        <BookiePicker bookie={currentBookie} handleClick={clickHandler} />
         <Standings data={apiData} />
         <Footer />
     </div>
--- src/components/BookiePicker.tsx
+++ src/components/BookiePicker.tsx
@@ -1,16 +1,21 @@
 import React, { FC } from "react";
 
-// change this to a button set of pill buttons, will look much better
+interface BookiePickerProps {
+    bookie: string,
+    handleClick: any;
+}
 
-const BookiePicker = () => {
+
+const BookiePicker: FC<BookiePickerProps> = ( {bookie, handleClick} ) => {
+    console.log(bookie);
     return (
         <> 
         <div>
-            <button className="bookie-button">Sky Bet</button>
-            <button className="bookie-button">PP/BF</button>
-            <button className="bookie-button">Ladbrokes</button>
-            <button className="bookie-button">William Hill</button>
-            <button className="bookie-button">Bet 365</button>
+            {bookie === 'Sky Bet' ? <button className="bookie-button-selected">Sky Bet</button> : <button className="bookie-button" onClick={() => {handleClick('Sky Bet')}}>Sky Bet</button>}
+            {bookie === 'PP/BF' ? <button className="bookie-button-selected">PP/BF</button> : <button className="bookie-button" onClick={() => {handleClick('PP/BF')}}>PP/BF</button>}
+            {bookie === 'Ladbrokes' ? <button className="bookie-button-selected">Ladbrokes</button> : <button className="bookie-button" onClick={() => {handleClick('Ladbrokes')}}>Ladbrokes</button>}
+            {bookie === 'William Hill' ? <button className="bookie-button-selected">William Hill</button> : <button className="bookie-button" onClick={() => {handleClick('William Hill')}}>William Hill</button>}
+            {bookie === 'Bet365' ? <button className="bookie-button-selected">Bet 365</button> : <button className="bookie-button" onClick={() => {handleClick('Bet 365')}}>Bet 365</button>}
         </div>
     </>
     )
