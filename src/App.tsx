eb4379db143f65bb8382ce4a7ba59d6aae958adc
Joe Ellis <joe.ellis@and.digital>
2021-11-30 14:48:09 +0000

    added more hcap data and endpoint builder

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,40 @@ import LoadingDots from './components/LoadingDots';
 
 // let dataPlaceholder: ApiData;
 
+// const league1Handler = () => {
+//   const league1Endpoint = 'https://v3.football.api-sports.io/standings?league=41&season=2021';
+//   const requestHeaders = {
+//     headers: {
+//       'x-apisports-key': '1ee142cfc34ceae31ba7758c4bd972f4',
+//     },
+//   };
+//   const { response, error, loading } = useFetch(league1Endpoint, requestHeaders);
+
+// }
+
+const endpointBuilder = (league: string) => {
+  let apiId;
+  switch (league) {
+    case 'PremierLeague':
+      apiId = 39;
+      break;
+    case 'Championship':
+      apiId = 40;
+      break;
+    case 'League 1':
+      apiId = 41;
+      break;
+    case 'League 2':
+      apiId = 42;
+      break;
+    default:
+      console.log('invalid league string provided');
+  }
+
+  const endpoint = `https://v3.football.api-sports.io/standings?league=${apiId}&season=2021`;
+  return endpoint;
+};
+
 const requestHeaders = {
   headers: {
     'X-Auth-Token': '05b09d4d6ebf494aae53d256c80fc85a',
